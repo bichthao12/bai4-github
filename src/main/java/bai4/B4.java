136d67270d4faaf736b51c14eccf3d25fb789ff0
bichthao12 <thaolahm9683@gmail.com>
2018-07-30 01:01:44 +0700

    Update b4 lan 3

--- src/main/java/bai4/B4.java
+++ src/main/java/bai4/B4.java
@@ -17,14 +17,14 @@ import com.google.common.cache.LoadingCache;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+
 public class B4 {
 	public static void main(String[] args) {
-/* Chinh sua hop nhat client va server*/
-		final LoadingCache<Integer, List<Integer>> primeCache =
-				CacheBuilder.newBuilder()
-				.maximumSize(100)
-				.expireAfterWrite(20, TimeUnit.SECONDS)
-				.expireAfterAccess(10, TimeUnit.SECONDS)
+		/* Chinh sua tren client */
+		final LoadingCache<Integer, List<Integer>> primeCache = 
+				CacheBuilder.newBuilder().maximumSize(100)
+				.expireAfterWrite(20, TimeUnit.SECONDS).
+				expireAfterAccess(10, TimeUnit.SECONDS)
 				.build(new CacheLoader<Integer, List<Integer>>() {
 					@Override
 					public List<Integer> load(Integer n) throws Exception {
@@ -33,25 +33,26 @@ public class B4 {
 					}
 				});
 		final JSONObject jo = new JSONObject();
-		get("/prime","application/json",new Route() {
-			public Object handle(Request request, Response respone) 
-					throws Exception {
-				 Integer n = Integer.parseInt(request.queryParams("n"));
-				jo.put("prime",primeCache.get(n));
-			//	jo.put("prime",primeCache.get(n));
-				return "Cache Size: "+primeCache.size()+" "+jo.toJSONString();
+		get("/prime", "application/json", new Route() {
+			public Object handle(Request request, Response respone) throws Exception {
+				Integer n = Integer.parseInt(request.queryParams("n"));
+				jo.put("prime", primeCache.get(n));
+				// jo.put("prime",primeCache.get(n));
+				return "Cache Size: " + primeCache.size() + " " + jo.toJSONString();
 			}
 		});
 	}
-	private static List<Integer> getAllPrime(Integer n) {	
-			List<Integer> listPrime = new ArrayList<Integer>();
-			for ( int i = 0; i <= n; i++) {
-				if (isPrime(i)) {
-					listPrime.add(i);
-				}
+
+	private static List<Integer> getAllPrime(Integer n) {
+		List<Integer> listPrime = new ArrayList<Integer>();
+		for (int i = 0; i <= n; i++) {
+			if (isPrime(i)) {
+				listPrime.add(i);
 			}
-			return listPrime;
+		}
+		return listPrime;
 	}
+
 	private static boolean isPrime(int n) {
 		if (n < 2) {
 			return false;
